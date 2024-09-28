d8bfa4e92e6f669b57c3fe2e89037c846ee88ab2
sharmakekahiye <kahiecode@gmail.com>
2024-09-28 14:46:09 +0300

    add SafeArea-Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("My app"),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Text(
-          'Hello, Flutter!',
-          style: TextStyle(fontSize: 24, color: Colors.blue),
+    home: SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("My app"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Text(
+            'Hello, Flutter!',
+            style: TextStyle(fontSize: 24, color: Colors.blue),
+          ),
         ),
       ),
     ),
